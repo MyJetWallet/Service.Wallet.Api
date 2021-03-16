29e7fcfc668ec86c668dbc1e94ae0dde7815a96e
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-03-16 13:53:50 +0200

    signalr stream price

--- src/Service.Wallet.Api/Jobs/PriceChangesNotificator.cs
+++ src/Service.Wallet.Api/Jobs/PriceChangesNotificator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Autofac;
 using DotNetCoreDecorators;
@@ -12,7 +13,7 @@ namespace Service.Wallet.Api.Jobs
 {
     public class PriceChangesNotificator : IStartable, IDisposable
     {
-        public int TimerDelayMs = 100;
+        public int TimerDelayMs = 500;
 
         private readonly IHubManager _hubManager;
         
@@ -39,6 +40,19 @@ namespace Service.Wallet.Api.Jobs
             _timer.Start();
         }
 
+
+        private Dictionary<string, decimal> _prices = new Dictionary<string, decimal>()
+        {
+            { "BTCUSD", 50000m },
+            { "ETHUSD", 1800m },
+            { "LTCUSD", 200m },
+            { "BTCEUR", 49000m },
+            { "ETHEUR", 1600m },
+            { "LTCEUR", 180m }
+        };
+
+        private Random _rnd = new Random();
+
         private async Task DoProcess()
         {
             Dictionary<string, BidAsk> last;
@@ -46,6 +60,31 @@ namespace Service.Wallet.Api.Jobs
             {
                 last = _lastPrices;
                 _lastPrices = new Dictionary<string, BidAsk>();
+            }
+
+            var dt = DateTime.UtcNow;
+
+            foreach (var symbol in _prices.Keys.ToList())
+            {
+                // 0.00001
+                // 0.00050
+                // 0.00100
+                var d = _rnd.Next(100) / 10000m - 0.0050m;
+
+                var price = _prices[symbol];
+
+                price = price + price * d;
+                price = Math.Round(price, 4);
+                _prices[symbol] = price;
+
+                last[symbol] = new BidAsk()
+                {
+                    Ask = (double) price,
+                    Bid = (double) price,
+                    Id = symbol,
+                    LiquidityProvider = "jetwallet",
+                    DateTime = dt
+                };
             }
 
             await _hubManager.ExecForeachConnection(ctx => ctx.SendPrices(last.Values));
