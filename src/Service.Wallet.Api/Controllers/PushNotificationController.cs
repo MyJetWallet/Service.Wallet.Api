7fce5a091c2aeee23f49fac83fff782c1f3af45a
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-06-03 17:30:48 +0300

    logging received token

--- src/Service.Wallet.Api/Controllers/PushNotificationController.cs
+++ src/Service.Wallet.Api/Controllers/PushNotificationController.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using Service.Wallet.Api.Controllers.Contracts;
 
 namespace Service.Wallet.Api.Controllers
@@ -9,6 +11,15 @@ namespace Service.Wallet.Api.Controllers
     [Route("/api/v1/push")]
     public class PushNotificationController:ControllerBase
     {
+        
+        private readonly ILogger<PushNotificationController> _logger;
+
+        public PushNotificationController(ILogger<PushNotificationController> logger)
+        {
+            _logger = logger;
+        }
+
+
         /// <summary>
         /// Placeholder for push notification token registration 
         /// </summary>
@@ -16,6 +27,7 @@ namespace Service.Wallet.Api.Controllers
         [Authorize]
         public async Task<Response> RegisterToken([FromBody] RegisterTokenRequest request)
         {
+            _logger.LogInformation("Received token {Token} and locale {UserLocale}", request.Token, request.UserLocale);
             return Contracts.Response.OK();
         }
     }
