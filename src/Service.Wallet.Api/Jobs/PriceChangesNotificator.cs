61b1a202cee5ec8a994b725262f5e57592bbdd26
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-06-03 14:18:45 +0300

    SPOTDEV-229: Removed fake price change generation

--- src/Service.Wallet.Api/Jobs/PriceChangesNotificator.cs
+++ src/Service.Wallet.Api/Jobs/PriceChangesNotificator.cs
@@ -48,78 +48,10 @@ namespace Service.Wallet.Api.Jobs
         {
             _timer.Start();
         }
-
-
-        private Dictionary<string, decimal> _prices = new Dictionary<string, decimal>()
-        {
-            { "BTCUSD", 50000m },
-            { "ETHUSD", 1800m },
-            { "LTCUSD", 200m },
-            { "BTCEUR", 49000m },
-            { "ETHEUR", 1600m },
-            { "LTCEUR", 180m },
-            { "Index", 1m },
-            { "ADAUSD", 1.2480m },
-            { "XRPUSD", 0.46m },
-            { "BATUSD", 0.9930m },
-            { "DOTUSD", 35.17m }
-        };
-
-        private Random _rnd = new Random();
-
+        
         private async Task DoProcess()
         {
-            Dictionary<string, BidAsk> last;
-            lock (_gate)
-            {
-                last = _lastPrices;
-                _lastPrices = new Dictionary<string, BidAsk>();
-            }
-
-            var dt = DateTime.UtcNow;
-
-            foreach (var symbol in _prices.Keys.Where(e => e != "Index").ToList())
-            {
-                // 0.00001
-                // 0.00050
-                // 0.00100
-                var d = _rnd.Next(100) / 10000m - 0.0050m;
-
-                var price = _prices[symbol];
-
-                price = price + price * d;
-                price = Math.Round(price, 4);
-                _prices[symbol] = price;
-
-                last[symbol] = new BidAsk()
-                {
-                    Ask = (double) price,
-                    Bid = (double) price,
-                    Id = symbol,
-                    LiquidityProvider = "jetwallet",
-                    DateTime = dt
-                };
-            }
-
-            {
-                var symbol = "Index";
-
-                var price = _prices["Index"] = _prices["Index"] + 1;
-                if (price > 1000000)
-                    _prices["Index"] = 1;
-
-
-                last[symbol] = new BidAsk()
-                {
-                    Ask = (double)price,
-                    Bid = (double)price,
-                    Id = symbol,
-                    LiquidityProvider = "jetwallet",
-                    DateTime = dt
-                };
-            }
-
-            await _hubManager.ExecForeachConnection(ctx => ctx.SendPrices(last.Values));
+            await _hubManager.ExecForeachConnection(ctx => ctx.SendPrices(_lastPrices.Values));
         }
 
         public void Dispose()
