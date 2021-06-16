d2d074d5d62430501f23f80661010d92d8e863f3
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-06-16 17:16:13 +0300

    add info controller

---
+++ src/Service.Wallet.Api/Controllers/Contracts/GetSessionInfoResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Service.Wallet.Api.Controllers.Contracts
+{
+    public class GetSessionInfoResponse
+    {
+        public bool EmailVerified { get; set; }
+        public bool PhoneVerified { get; set; }
+        public bool TwoFactorAuthentication { get; set; }
+        public string TokenLifetimeRemaining { get; set; }
+    }
+}
---
+++ src/Service.Wallet.Api/Controllers/InfoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyJetWallet.Sdk.Authorization.Http;
+using Service.Wallet.Api.Controllers.Contracts;
+using Service.Wallet.Api.Domain.Wallets;
+
+namespace Service.Wallet.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("/api/v1/info")]
+    public class InfoController : ControllerBase
+    {
+        private readonly IWalletService _walletService;
+
+        public InfoController(IWalletService walletService)
+        {
+            _walletService = walletService;
+        }
+
+        /// <summary>
+        /// Create limit order on the wallet
+        /// Errors:
+        ///  * NoqEnoughLiquidityForConvert
+        /// </summary>
+        [HttpGet("session-info")]
+        public async Task<Response<GetSessionInfoResponse>> GetSessionInfoAsync()
+        {
+            await _walletService.GetDefaultWalletAsync(this.GetClientIdentity());
+
+            var tokenStr = this.GetSessionToken();
+            var (_, token) = MyControllerBaseHelper.ParseToken(tokenStr);
+
+            var response = new GetSessionInfoResponse()
+            {
+                EmailVerified = token.EmailVerified,
+                PhoneVerified = false,
+                TwoFactorAuthentication = token.Passed2FA,
+                TokenLifetimeRemaining = (token.Expires - DateTime.UtcNow).ToString()
+            };
+
+            return new Response<GetSessionInfoResponse>(response);
+        }
+    }
+}
